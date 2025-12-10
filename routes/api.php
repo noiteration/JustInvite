84813f1d1b1b708d56c9fc66af9a2eacb0f6f5b8
sahirp <47252018+noiteration@users.noreply.github.com>
2025-12-10 17:39:00 -0500

    [feat] API login feature implemented

--- routes/api.php
+++ routes/api.php
@@ -5,9 +5,14 @@ use Illuminate\Support\Facades\Route;
 
 // Controllers Imports
 use App\Http\Controllers\InvitationController;
+use App\Http\Controllers\API\ApiLoginController;
+
+
+Route::post('/login', [ApiLoginController::class, 'login'])->name('api.login');
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
 Route::post('/invite', [InvitationController::class, 'store']);
+
